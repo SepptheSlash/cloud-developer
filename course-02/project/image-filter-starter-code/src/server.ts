2cbcabe4719b12bed62180ad1f87a39dee157bc6
Sebastian Mayer <seb@MacBook-Pro-von-Sebastian.fritz.box>
2022-02-28 11:07:34 +0100

    Finished Project

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+//import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import fs from 'fs';
 
 (async () => {
 
@@ -9,26 +11,39 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
+
+  //set the directory where temporary files are stored
+  const folder_to_clean = __dirname + '/util/tmp/'
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
+    let { image_url } = req.query;
 
-  /**************************************************************************** */
-
+    //    1. validate the image_url query
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`url is required`);
+    }
+    
+    //    2. call filterImageFromURL(image_url) to filter the image
+    const filteredpath = await filterImageFromURL(image_url);
+    
+    //    3. send the resulting file in the response
+    res.status(200).sendFile(filteredpath);
+    
+    //    4. deletes any files on the server on finish of the response
+    return res.on('finish', function() {
+      let delete_array : string[] = [];
+      fs.readdirSync(folder_to_clean).forEach(file => {
+          delete_array.push(folder_to_clean + file);
+      });
+      deleteLocalFiles(delete_array);
+    });
+    
+  } );
   //! END @TODO1
   
   // Root Endpoint
